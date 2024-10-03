861649554ee03dc7275488b7b0022bcca99354e2
Nahid Anwar <91697841+nahidanwar007@users.noreply.github.com>
2024-10-03 16:29:42 -0600

    Time: 82 ms (87.45%) | Memory: 22.1 MB (7.64%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,9 +1,28 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        mmap = {}
+        # mmap = {}
+        # for n in nums:
+        #     mmap[n] = 1 + mmap.get(n, 0)
+        
+        # mmap = sorted(mmap.items(), key=lambda item: item[1], reverse=True)
+        
+        # return [item[0] for item in mmap[:k]]
+
+        # bucket sort solution
+
+        count = {}
         for n in nums:
-            mmap[n] = 1 + mmap.get(n, 0)
+            count[n] = 1 + count.get(n, 0)
         
-        mmap = sorted(mmap.items(), key=lambda item: item[1], reverse=True)
+        freq = [[] for i in range(len(nums)+1)]
+
+        for key, val in count.items():
+            freq[val].append(key)
         
-        return [item[0] for item in mmap[:k]]
+        res = []
+
+        for i in range(len(freq)-1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
